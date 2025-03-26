6dd2558a744cde8f4219115629763e8851b939ba
rlawjdgns02 <kinddevil02@gmail.com>
2025-03-26 17:14:57 +0900

    초기 설정

---
+++ src/main/java/maite/maite/MaiteApplication.java
@@ -0,0 +1,13 @@
+package maite.maite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maite/maite/MaiteApplicationTests.java
@@ -0,0 +1,13 @@
+package maite.maite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
