14bcd60527c030c8488b9ea44151766862281436
whiteys1 <147918555+whiteys1@users.noreply.github.com>
2025-03-27 23:53:48 +0900

    Add application.yml secret integration

---
+++ src/main/java/maite/maite/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package maite.maite.repository;
+
+import maite.maite.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
