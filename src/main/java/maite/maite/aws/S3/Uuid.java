b0f4d0ddb09dd17b62fdaa0b1fd220e45206dcbd
whiteys1 <147918555+whiteys1@users.noreply.github.com>
2025-05-03 21:38:15 +0900

    s3제거

--- src/main/java/maite/maite/aws/S3/Uuid.java
+++ src/main/java/maite/maite/aws/S3/Uuid.java
@@ -1,21 +1,21 @@
-package maite.maite.aws.S3;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Builder
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-public class Uuid {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(unique = true)
-    private String uuid;
-}
+//package maite.maite.aws.S3;
+//
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Builder
+//@Getter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class Uuid {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(unique = true)
+//    private String uuid;
+//}
